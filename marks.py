cb7e8c3ef5a23a88a9efc1f304f06a6245a10780
Chauhan Kamlesh <kamlesh363421@gmail.com>
2026-02-09 10:29:23 +0530

    Add files via upload

---
+++ marks.py
@@ -0,0 +1,9 @@
+marks = int(input("Enter your marks :"))
+if marks>=90 :
+    print("you will get A grade")
+elif marks>=70 :
+    print("you will get B grade")
+else :
+    print("you will get C grade")
+     
+            
