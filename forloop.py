ed964bf6211f545468f5c07934be301c9b81a07d
Chauhan Kamlesh <kamlesh363421@gmail.com>
2026-02-16 10:32:49 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,43 @@
+"""1.print number from 1 to 10
+for i in range (1,11) :
+    print(i)"""
+
+"""2.print even number from 1 to 20
+for i in range(1,21):
+    if i % 2 == 0 :
+        print(i)"""
+
+"""3.print odd number from 1 to 15
+for i in range (1,16):
+    if i % 2 !=0 :
+        print(i)"""
+
+"""4.print table of 5
+for i in range (1,11):
+    print("5x",i,"=",5*i)"""
+
+"""5.print characters of a string
+name="ck"
+for letter in name :
+    print(letter)"""
+
+"""6.sum of numbers from 1 to 5.
+total=0
+for i in range (1,6):
+    total=total+i
+    print("sum is :",total)"""
+
+
+"""7.print list element
+number=[10,20,30,40]
+for n in number:
+    print(n) """
+
+"""8.print numbers from 1 to 5
+for i in range (1,6):
+    print(i)"""
+
+#9.print a message 3 time
+for i in range(3):
+      print("hello")
+    
