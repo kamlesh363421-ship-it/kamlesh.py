965ca96f4d7dffdb44a07baa738656111afddfd9
Chauhan Kamlesh <kamlesh363421@gmail.com>
2026-02-25 08:20:39 +0530

    Add files via upload

---
+++ whileloop.py
@@ -0,0 +1,46 @@
+"""1. print number from 1 to 5
+i=1
+while i<=5 :
+    print (i)
+    i=i+1"""
+
+"""2.sum of number take user input
+n=int(input("enter number :"))
+i=1
+s=0
+while i<=n:
+    s=s+i
+    i=i+1
+    print("sum=",s)"""
+
+"""3.print odd number between 1 and 20
+i=1
+while i<=20 :
+    if i % 2 !=0 :
+        print(i)
+    i +=1"""
+
+"""4.print table of 4 
+num=int(input("enter number :"))
+i=1
+while i<=10 :
+    print(num,"x",i,"=",num*i)
+    i=i+1"""
+
+"""5.print revers number
+i=10
+while i>=1 :
+    print(i)
+    i=i-1"""
+
+"""6.find largest number in list
+num=[10,20,30,40]
+largest=max(num)
+print(largest)"""
+
+#7.print even number between 1 to 20 
+i=1
+while i<=20 :
+    if i % 2 ==0 :
+        print(i)
+    i +=1
