19fba41f566bc9a0930869c6be1cb476701febf1
Chauhan Kamlesh <kamlesh363421@gmail.com>
2026-03-09 19:21:32 +0530

    Add files via upload

---
+++ Array3.py
@@ -0,0 +1,38 @@
+""""1. basic slics
+
+from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[1:4])
+print(arr[:3])
+print(arr[2:3])
+print(arr[:])"""
+
+"""2.slicing with step
+
+from array import array
+arr=array('i',[10,20,30,40,50,60,70,80])
+print(arr[::2])
+print(arr[1::2])
+print(arr[::3])"""
+
+"""3.negative slicing
+
+from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[-4:-1])
+print(arr[-3:])
+print(arr[:-2])"""
+
+"""4.reverse array
+
+from array import array
+arr=array('i',[10,20,30,40,50])
+print(arr[::-1])"""
+
+#5.modifying slices
+
+from array import array
+arr=array('i',[10,20,30,40,50])
+arr[1:4]=array('i',[25,35,45])
+print(arr)
+
