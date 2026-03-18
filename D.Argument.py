3205fa7a8b922a6015417926049edafa8356fb00
Chauhan Kamlesh <kamlesh363421@gmail.com>
2026-03-18 08:49:47 +0530

    Add files via upload

---
+++ D.Argument.py
@@ -0,0 +1,24 @@
+"""1.square of number
+
+def sqr(num,exp=2):
+    return num ** exp
+
+print(sqr(3))
+print(sqr(3,3))
+print(sqr(2,4))"""
+
+"""2.
+ 
+def greet (name="guest"):
+    print("hello",name)
+    
+greet("alice")
+greet()"""
+
+#3.
+
+def add (a,b=5):
+    print("sum :",a+b)
+    
+add(10,20)
+add(10)
